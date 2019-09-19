3d966c410982ae010c2a54961faa7f17d8efdfe5
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-09-19 02:43:22 -0700

    OK! Got physics appearing

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,25 @@
+import { World, Vec2, Edge, Circle } from "planck-js";
+
 export default class Controller {
 
 	constructor() {
 		this.animAmt = 0;
 		this.period = 3;
+
+		this.lastStep = 0;
+		this.timeCount = 0;
+		this.stepTime = 1 / 60;
+		this.numSteps = 0;
+
+		this.world = World({gravity: Vec2(0, -10)})
+
+		const ground = this.world.createBody({
+			type: 'static',
+			position: Vec2(0, -10),
+		});
+		ground.createFixture({
+			shape: Edge(Vec2(-40.0, 0.0), Vec2(40.0, 0.0))
+		});
 	}
 
 	/**
@@ -13,6 +30,22 @@ export default class Controller {
 	update(dt) {
 		this.animAmt += dt / this.period;
 		this.animAmt %= 1;
+
+		this.timeCount += dt;
+		for (let i = 0; i < 10 && this.lastStep < this.timeCount; i++) {
+			this.physicsStep();
+
+			this.lastStep += this.stepTime;
+			this.numSteps ++;
+		}
+	}
+
+	physicsStep() {
+		if (this.numSteps % 60 == 0) {
+			const circleBody = this.world.createBody({type: 'dynamic', position: Vec2(0, 10)});
+			circleBody.createFixture(Circle(1), {density: 1});
+		}
+		this.world.step(this.stepTime);
 	}
 
 	/**
@@ -21,14 +54,23 @@ export default class Controller {
 	 * @param {!CanvasRenderingContext2D} context
 	 */
 	render(context) {
-		context.beginPath();
-		context.fillStyle = 'black';
-		context.moveTo(0, 0);
-		context.arc(0, 0, 100, 0, 2 * Math.PI * this.animAmt);
-		context.fill();
-
-		context.scale(10, 10);
-		context.fillText(this.period * this.animAmt, 0, 0);
+		context.scale(10, -10);
+		for (var body = this.world.getBodyList(); body; body = body.getNext()) {
+			const bodyPos = body.getPosition();
+			for (var fixture = body.getFixtureList(); fixture; fixture = fixture.getNext()) {
+				const shape = fixture.getShape();
+				if (shape.getType() != 'circle') {
+					continue;
+				}
+				const center = shape.getCenter();
+				const radius = shape.getRadius();
+			
+				context.beginPath();
+				context.fillStyle = 'black';
+				context.arc(bodyPos.x + center.x, bodyPos.y + center.y, radius, 0, 2 * Math.PI);
+				context.fill();
+			}
+		}
 	}
 
 }
